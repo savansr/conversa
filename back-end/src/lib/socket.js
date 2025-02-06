b6670e04cb4bad2491fcb3f7d4e7be6e3e4046f4
savansr <Savansr26@gmail.com>
2025-02-06 12:48:33 +0530

    Update socket.js

--- back-end/src/lib/socket.js
+++ back-end/src/lib/socket.js
@@ -8,6 +8,9 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: ["https://conversa-the-chat-app.netlify.app","http://localhost:3000"],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   },
 });
 
