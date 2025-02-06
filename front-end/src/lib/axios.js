841447c8b9b1593259652298fd6b25b58197d388
savansr <Savansr26@gmail.com>
2025-02-06 23:25:04 +0530

    Update axios.js

--- front-end/src/lib/axios.js
+++ front-end/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:5002/api",
+    baseURL: "https://conversa-backend-liwy.onrender.com/api",
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
